d30f15e03ea3631ab9ba64d2e606aeb081446aaf
melkaj <49014507+melkaj@users.noreply.github.com>
2020-03-20 04:46:33 -0400

    Cleaned up some code

--- definitions.py
+++ definitions.py
@@ -1,12 +1,4 @@
 
-# Helper function
-def sum_of_lists(list1):
-    total = 0
-    for elem in list1:
-        total += elem
-    return total
-
-
 # boolean function to find out if two strings can be 
 # mapped one to one
 def is_one_to_one(str1, str2):
@@ -34,18 +26,7 @@ def is_one_to_one(str1, str2):
 
     # If amount of unique characters in each string match,
     # then it can be mapped one to one
-    if (amount_of_unique_char_str1 == amount_of_unique_char_str2):
+    if (amount_of_unique_char_str1 >= amount_of_unique_char_str2):
         return True
     else: 
-        list_str2 = list(dict_str2.values())
-        # for the when the amount of unique characters in string1 is greater than string 2
-        # Then we have to check if the amount of unique characters in string 1 is less than 
-        # the total characters in string 2
-        # (ex: hel - php)
-        if (amount_of_unique_char_str1 >= amount_of_unique_char_str2
-             and amount_of_unique_char_str1 <= sum_of_lists(list_str2)):
-            return True
-        else:
-            return False
-
-
+        return False

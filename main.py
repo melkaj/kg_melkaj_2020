f8e42fde0da67359ada6197a41bdd70b9da9a3c4
melkaj <49014507+melkaj@users.noreply.github.com>
2020-03-20 04:09:59 -0400

    cleaned up some debug comments and added some saftey nets regarding the amount of command line arguments

--- main.py
+++ main.py
@@ -6,22 +6,16 @@ import sys
 from definitions import is_one_to_one
 
 # Getting strings from the command line
-str1 = sys.argv[1]
-str2 = sys.argv[2]
+if len(sys.argv) == 3:
+    str1 = sys.argv[1]
+    str2 = sys.argv[2]
 
-# myDict = {}
-# myDict['r'] = 3
-# print(myDict['r'])
-# myDict['r'] = myDict['r'] + 1
-# print(myDict['r'])
+    # Getting final answer and printing it out to the console
+    final_answer = is_one_to_one(str1, str2)
+    print(f'\n{final_answer}')
 
-# myDict = {'4': 4, '5': 5, '6': 6}
-# print(len(myDict))
-
-
-# Getting final answer and printing it out to the console
-final_answer = is_one_to_one(str1, str2)
-print(f'\n{final_answer}')
+else:
+    print("\nRetry with the correct number of arguments please")
 
 
 

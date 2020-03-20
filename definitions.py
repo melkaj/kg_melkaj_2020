7d76c72c8a5561b344eed571fd37a344d4340bb6
melkaj <49014507+melkaj@users.noreply.github.com>
2020-03-20 04:51:41 -0400

    changed dictionaries to lists since values were not needed in my implementation

--- definitions.py
+++ definitions.py
@@ -3,26 +3,22 @@
 # mapped one to one
 def is_one_to_one(str1, str2):
     # Initializing empty dictionaries
-    dict_str1 = {}
-    dict_str2 = {}
+    list_str1 = []
+    list_str2 = []
 
     # Iterate through the first string to get count of unique characters
     for char in str1:
-        if dict_str1.get(char) == None:
-            dict_str1[char] = 1
-        else:
-            dict_str1[char] = dict_str1[char] + 1    
+        if char not in list_str1:
+            list_str1.append(char)    
         
     # Iterate through the second string to get count of unique characters
     for char in str2:
-        if dict_str2.get(char) == None:
-            dict_str2[char] = 1
-        else:
-            dict_str2[char] = dict_str2[char] + 1
-
+        if char not in list_str2:
+            list_str2.append(char)
+        
     # Getting amount of unique characters in each string
-    amount_of_unique_char_str1 = len(dict_str1)
-    amount_of_unique_char_str2 = len(dict_str2)
+    amount_of_unique_char_str1 = len(list_str1)
+    amount_of_unique_char_str2 = len(list_str2)
 
     # If amount of unique characters in each string match,
     # then it can be mapped one to one
